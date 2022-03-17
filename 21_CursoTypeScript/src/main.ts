f9c9c19584e17062829c04fb1c679e8e9dde35fc
Andres Pacheco <aaps470@gmail.com>
2022-03-17 11:59:46 -0400

    Proyecto Lading Page 2

---
+++ 21_CursoTypeScript/src/main.ts
@@ -0,0 +1,16 @@
+console.log('Hola TypeScript con Platzi world');
+
+/* #Tipado en TypeScript:
+
+Explicito: Define una sintaxis para la creación de variables con tipo de dato.
+
+nomVariable : Tipo de dato
+
+Inferido: TypeScript tiene la habilidad de deducir el tipo en funcion de un valor.
+
+
+#Tipo de datos primitivos:
+
+Number > Boolean > String > Array
+Tuple > Enum > Any > Void
+Null > Undefined > Never > Object*/
